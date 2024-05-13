bb1a3f9fc15219f8ec649e04371cad500ead3702
vuvanhoang <hoangvuvan677@gmail.com>
2024-05-13 23:08:53 +0700

    newss

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -8,20 +8,20 @@ export async function POST(req: Request, res: Response) {
   const { email, subject, message } = await req.json();
   console.log(email, subject, message);
 
-  try {
-    const { data, error } = await resend.emails.send({
-      from: 'Acme <onboarding@resend.dev>',
-      to: ['hoangvuvan677@gmail.com', email],
-      subject: 'Hello world',
-      react: EmailTemplate({ firstName: 'John' }),
-    });
+  // try {
+  //   const { data, error } = await resend.emails.send({
+  //     from: 'Acme <onboarding@resend.dev>',
+  //     to: ['hoangvuvan677@gmail.com', email],
+  //     subject: 'Hello world',
+  //     react: EmailTemplate({ firstName: 'John' }),
+  //   });
 
-    if (error) {
-      return Response.json({ error }, { status: 500 });
-    }
+  //   if (error) {
+  //     return Response.json({ error }, { status: 500 });
+  //   }
 
-    return Response.json(data);
-  } catch (error) {
-    return Response.json({ error }, { status: 500 });
-  }
+  //   return Response.json(data);
+  // } catch (error) {
+  //   return Response.json({ error }, { status: 500 });
+  // }
 }
