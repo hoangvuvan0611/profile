e226323373174aa8efb2e2d06108d686689621d9
vuvanhoang <hoangvuvan677@gmail.com>
2024-05-14 16:38:40 +0700

    app name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <title>VuVanHoang</title>
       <body className={inter.className}>{children}</body>
     </html>
   );
