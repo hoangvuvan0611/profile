166d9544deaec61f72f8f7ae7697d0b50cfeabfd
vuvanhoang <hoangvuvan677@gmail.com>
2024-05-14 17:04:56 +0700

    img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,19 @@ import HeroSection from "@/components/hero/HeroSection";
 import Navbar from "@/components/navbar/Navbar";
 import ProjectSection from "@/components/project/ProjectSection";
 
+const prefix = process.env.NEXT_PUBLIC_BASE_PATH + "/images/banner-bg.png"|| '/images/banner-bg.png';
+
 export default function Home() {
+
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
-      <div className="bg-[url('/images/banner-bg.png')] bg-left-center bg-cover bg-no-repeat h-[100vh]">
-        <Navbar />
+      <div className=" bg-left-center bg-cover bg-no-repeat h-[100vh]" 
+        style={{ backgroundImage: `url(${prefix})` }}
+      >
+        <Navbar/>
         <HeroSection />
       </div>
-      <div className="container">
+      <div className="container" >
         <AboutSection />
         <ProjectSection />
         <EmailSection />
