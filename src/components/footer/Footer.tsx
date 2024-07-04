afb8a6597db13b011b13fd5e6ba61da48203c063
Vũ Văn Hoàng <6656485@sv.vnua.edu.vn>
2024-07-04 09:55:19 +0700

    update mobile project

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,9 +4,9 @@ const Footer = () => {
   return (
     <div>
         <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-            <div className="container p-12 flex justify-between">
+            <div className="container p-12 d-flex justify-between">
                 <span>SIRIUS</span>
-                <p className="text-slate-600">Copy right VuVanHoang - 2024</p>
+                <p className="text-slate-600">Developed in with love - 2024</p>
             </div>
         </footer>
     </div>
